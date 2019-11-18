ffc4627c927be09af9698a8800b11a88f9e4cc4e
Oscar Hult <oscar@tessin.com>
2019-11-18 18:06:04 +0100

    🍺

---
+++ install.sh
@@ -0,0 +1,28 @@
+brew cask install visual-studio-code
+brew cask install chromium
+brew cask install docker
+brew cask install github
+brew cask install aerial
+brew cask install signal
+brew cask install cakebrew
+brew cask install postman
+brew cask install slack
+brew cask install spotify
+brew cask install discord
+brew cask install rectangle
+brew cask install dotnet
+
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code
+
+brew install node
+brew install git
+brew install yarn
+brew install wget
+brew install go
+brew install watch
+brew install htop
+brew install zsh-autosuggestions
+
+echo '$(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
+source ~/.zshrc
