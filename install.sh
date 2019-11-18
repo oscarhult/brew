27bc504bab550f21527459693ae60527f56dc5f5
Oscar Hult <oscar@hult.it>
2019-11-18 19:17:17 +0100

    Added java, bitwarden, openemu, firefox, hacker-menu, plex-media-player, mongodb-compass-community. Removed cakebrew

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 brew cask install visual-studio-code
 brew cask install google-chrome
+brew cask install firefox
 brew cask install chromium
 brew cask install docker
 brew cask install github
 brew cask install aerial
 brew cask install signal
-brew cask install cakebrew
 brew cask install postman
 brew cask install slack
 brew cask install spotify
@@ -14,11 +14,18 @@ brew cask install rectangle
 brew cask install dotnetb
 brew cask install azure-data-studio
 brew cask install microsoft-azure-storage-explorer
+brew cask install java
+brew cask install bitwarden
+brew cask install openemu
+brew cask install hacker-menu
+brew cask install plex-media-player
+brew cask install mongodb-compass-community
 
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
 
 brew install azure-cli
+brew install qemu
 brew install node
 brew install git
 brew install yarn
