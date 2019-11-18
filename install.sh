0fd9454b01cc2e77df5cef467b5823a8b96a8319
Oscar Hult <oscar@hult.it>
2019-11-18 19:22:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,38 +1,38 @@
-brew cask install visual-studio-code
-brew cask install google-chrome
-brew cask install firefox
-brew cask install chromium
-brew cask install docker
-brew cask install github
 brew cask install aerial
-brew cask install signal
+brew cask install azure-data-studio
+brew cask install bitwarden
+brew cask install chromium
+brew cask install discord
+brew cask install docker
+brew cask install dotnet
+brew cask install firefox
+brew cask install github
+brew cask install google-chrome
+brew cask install hacker-menu
+brew cask install java
+brew cask install microsoft-azure-storage-explorer
+brew cask install mongodb-compass-community
+brew cask install openemu
+brew cask install plex-media-player
 brew cask install postman
+brew cask install rectangle
+brew cask install signal
 brew cask install slack
 brew cask install spotify
-brew cask install discord
-brew cask install rectangle
-brew cask install dotnetb
-brew cask install azure-data-studio
-brew cask install microsoft-azure-storage-explorer
-brew cask install java
-brew cask install bitwarden
-brew cask install openemu
-brew cask install hacker-menu
-brew cask install plex-media-player
-brew cask install mongodb-compass-community
+brew cask install visual-studio-code
 
 brew tap homebrew/cask-fonts
 brew cask install font-fira-code
 
 brew install azure-cli
-brew install qemu
-brew install node
 brew install git
-brew install yarn
-brew install wget
 brew install go
-brew install watch
 brew install htop
+brew install node
+brew install qemu
+brew install watch
+brew install wget
+brew install yarn
 brew install zsh-autosuggestions
 
 echo 'source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh' >> ~/.zshrc
